f69cdaf587e69614b8d2e7fd1edc3e9194b3a423
danivelop <mofg266@gmail.com>
2021-08-12 20:40:43 +0900

    fixed wrapper에서 html에 height 100% 제거하고 position속성으로 해결

--- src/components/Chat/Chat.styled.ts
+++ src/components/Chat/Chat.styled.ts
@@ -2,8 +2,11 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
-  width: 100%;
-  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
   overflow-y: auto;
   background-color: white;
 `
