0ad30dc668699dd4836d1bbb6dcefe7191bf7719
danivelop <mofg266@gmail.com>
2021-08-12 22:58:14 +0900

    position으로 해결했던거 다시 html height 100%로 변경

--- src/components/Chat/Chat.styled.ts
+++ src/components/Chat/Chat.styled.ts
@@ -2,11 +2,8 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
+  width: 100%;
+  height: 100%;
   overflow-y: auto;
   background-color: white;
 `
